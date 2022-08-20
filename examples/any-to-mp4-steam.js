12cd81d60a2ba57c95653a9e61c94ab6a3e4197d
Kevin McDonagh <hello@kevinmcdonagh.name>
2022-08-20 16:17:58 +0100

    Getting demos working
    Added @ffmpeg-installer/ffmpeg & fluent-ffmpeg which make it easier to get ffmpeg up and running

--- examples/any-to-mp4-steam.js
+++ examples/any-to-mp4-steam.js
@@ -4,13 +4,26 @@
 
 var fs = require('fs');
 var path = require('path');
-var ffmpeg = require('../index');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 var pathToSourceFile = path.resolve(__dirname, '../test/assets/testvideo-169.avi');
 var readStream = fs.createReadStream(pathToSourceFile);
 var writeStream = fs.createWriteStream('./output.mp4');
 
 ffmpeg(readStream)
+    .on('progress', function(progress) {
+        console.log('Processing: ' + progress.percent + '% done @ ' + progress.currentFps + ' fps');
+    })
+    .on('end', function(err, stdout, stderr) {
+        console.log('Finished processing', err, stdout, stderr);
+    })
+    .on('error', function(err, stdout, stderr) {
+        console.log(err.message); //this will likely return "code=1" not really useful
+        console.log("stdout:\n" + stdout);
+        console.log("stderr:\n" + stderr); //this will contain more detailed debugging info
+    })
   .addOutputOptions('-movflags +frag_keyframe+separate_moof+omit_tfhd_offset+empty_moov')
   .format('mp4')
   .pipe(writeStream);
